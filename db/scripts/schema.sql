aa4f200fed7ba7e9bdc6e82baec5ada6c965f0ae
Wayne <wayne.c.griffiths@gmail.com>
2020-05-17 13:36:31 +0800

    rolled prompts.update and prompts.delete into prompts.select with switch logic instead

--- db/scripts/schema.sql
+++ db/scripts/schema.sql
@@ -5,18 +5,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-department_id INT AUTO_INCREMENT,
+id INT AUTO_INCREMENT,
 department_name VARCHAR(30) NOT NULL,
-PRIMARY KEY(department_id)
+PRIMARY KEY(id)
 );
 
 CREATE TABLE employee_role (
-role_id INT AUTO_INCREMENT,
+id INT AUTO_INCREMENT,
 title VARCHAR(30) NOT NULL,
-salary VARCHAR(30) NOT NULL,
+salary INT NOT NULL,
 department_id INT NOT NULL,
-PRIMARY KEY(role_id),
-FOREIGN KEY(department_id) REFERENCES department(department_id)
+PRIMARY KEY(id),
+FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE manager (
+id INT AUTO_INCREMENT, 
+manager_name VARCHAR(30) NOT NULL,
+PRIMARY KEY(id)
 );
 
 CREATE TABLE employee (
@@ -26,11 +32,6 @@ last_name VARCHAR(30) NOT NULL,
 role_id INT,
 manager_id INT,
 PRIMARY KEY(id),
-FOREIGN KEY(role_id) REFERENCES employee_role(role_id)
-);
-
-CREATE TABLE manager (
-manager_id INT NOT NULL, 
-manager_name VARCHAR(30),
-PRIMARY KEY(manager_id)
+FOREIGN KEY(role_id) REFERENCES employee_role(id),
+FOREIGN KEY(manager_id) REFERENCES manager(id)
 );
